6723ffa94fdb1686466be346e9fe4ea167fe9b75
Xavi Gimenez <xavi.gimenez@sirisacademic.com>
2021-05-19 11:54:42 +0200

    Getting data from OWID. Prototyping some ideas about evolution during the last year in terms of correlations (mobility vs metrics)

--- EDA/main.R
+++ EDA/main.R
@@ -25,3 +25,50 @@ write.csv(
   sep = ",",
   row.names = FALSE
 )
+
+
+# get data from Our World In Data
+owid_covid_data <- read.csv2(
+  file = "data/owid-covid-data.csv",
+  header = TRUE,
+  sep = ",",
+  stringsAsFactors = FALSE
+)
+
+# just get date, reproduction rate and filter by dates with indicator values
+reproduction_rate_ES <- owid_covid_data %>% 
+  filter(location == "Spain") %>%
+  select(date, reproduction_rate) %>%
+  mutate( reproduction_rate = as.numeric(reproduction_rate)) %>%
+  filter(!is.na(reproduction_rate))
+
+# get only mobility data at country level
+df_ES.all_regions_agg <- df_ES %>%
+  filter(sub_region_1 == "") %>%
+  select(
+    date, 
+    retail_and_recreation_percent_change_from_baseline,
+    grocery_and_pharmacy_percent_change_from_baseline,
+    parks_percent_change_from_baseline,
+    transit_stations_percent_change_from_baseline,
+    workplaces_percent_change_from_baseline,
+    residential_percent_change_from_baseline
+  )
+
+
+all_data <- inner_join(
+    reproduction_rate_ES,
+    df_ES.all_regions_agg,
+    by = "date"
+  ) %>%
+  # make it tidy
+  pivot_longer(
+    cols = ends_with("_from_baseline"),
+    names_to = "metric",
+    values_to = "mobility_change_from_baseline"
+  )
+
+write_json(
+  x = all_data,
+  path = "data_output/ES_reproductionrate_vs_mobility.json"
+)

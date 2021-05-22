db7d01449028fb62fc43876d56fe6552942679c1
Xavi Gimenez <xavi.gimenez@sirisacademic.com>
2021-05-22 18:49:23 +0200

    Bootstrapping web interactive

---
+++ web-interactive/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+        <h1>Bootstraping</h1>
+    </Container>    
+  );
+}
+
+export default App;
---
+++ web-interactive/src/common/constants.js
@@ -0,0 +1,13 @@
+
+export const MOBILITY_CATEGORIES = {
+    "retail_and_recreation_percent_change_from_baseline":   "Retail and recreation",
+    "grocery_and_pharmacy_percent_change_from_baseline":    "Grocery and pharmacy",
+    "parks_percent_change_from_baseline":                   "Parks",
+    "transit_stations_percent_change_from_baseline":        "Transit stations",
+    "workplaces_percent_change_from_baseline":              "Workplaces",
+    "residential_percent_change_from_baseline":             "Residential"   
+};
+
+export const COLORS = {
+    BG: '#f8f8ee'
+}
